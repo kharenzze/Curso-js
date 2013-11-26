2a26217437c6045e40bb0dde1987f75d641bd41d
paolo <paolo@localhost.localdomain>
2013-11-26 18:06:42 +0100

    ubiendo servidor

---
+++ tema 3/servidor1.js
@@ -0,0 +1,19 @@
+fs = require('fs');
+var documento;
+fs.readFile('index.html', 'utf8', 
+	    function(err,datos) {
+		if (err) {
+		    return console.log(err);
+		};
+		documento=datos;
+		});
+
+var http=require('http');
+var contador=0;
+http.createServer(function (req, res) {
+ // res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
+  res.end(documento);
+  contador++;
+  console.log(contador);
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
