de0742f6f04955189a25378c4d359fb9f5a85491
kharenzze <str237@gmail.com>
2013-12-12 05:14:50 +0100

    Añadida entrada y zona de usuario

--- proyecto/servidor.js
+++ proyecto/servidor.js
@@ -11,19 +11,23 @@ app.get('/', function (req, res) {
 	res.send(leer('index.html'));
 });
 
-app.get('/proc', function (req, res) {   
-	res.send('No es la portada');
+app.get('/entrar', function (req, res) {   
+	res.send(leer('entrar.html'));
 });
 
 app.get('/new', function (req, res) {   
 	res.send(leer('new.html'));
 });
 
-app.post('/log/:id/:id2', function (req, res) { 
+app.get('/zona', function (req, res) {   
+	res.send(leer('PaginaUsuario.html'));
+});
+
+app.put('/log/:id/:id2', function (req, res) { 
 	usuario=req.params.id;
 	pass=req.params.id2;
 	if (registro[usuario]==undefined){	
-		registro[usuario]=pass;
+		registro[usuario]={'password': pass, 'puntos':0};
 		console.log('Nuevo usuario añadido\n'+req.params.id);
 		res.send('1');
 		console.log(registro);
@@ -32,6 +36,23 @@ app.post('/log/:id/:id2', function (req, res) {
 		res.send('0');
 		console.log(registro);
 	}
+});
+
+app.post('/log/:id/:id2', function (req, res) { 
+	usuario=req.params.id;
+	pass=req.params.id2;
+	if (registro[usuario].password==pass){	
+		console.log('Nuevo acceso');
+		res.send('1');
+	}
+	else{
+		res.send('0');
+	}
+});
+
+app.get('/ranking/:id', function (req, res) {
+	res.send('{"puntos" : '+registro[req.params.id].puntos+' }\n');
+	console.log(registro[req.params.id].puntos);
 });
 
 var port=process.env.PORT || 5000;
